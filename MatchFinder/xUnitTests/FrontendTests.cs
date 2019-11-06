d07970190493b2ffb853f5d3de7a9d141304091e
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-11-06 13:05:19 +0100

    LoadMainLabel unit test

--- MatchFinder/xUnitTests/FrontendTests.cs
+++ MatchFinder/xUnitTests/FrontendTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MatchFinder;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xunit;
 
 namespace xUnitTests
@@ -21,6 +22,20 @@ namespace xUnitTests
             string secondText = second.MainLabel.Text;
 
             Assert.Equal(secondText, firstText);
+        }
+
+        [Fact]
+        public void LoadMainLabelTest()
+        {
+            Frontend front = Frontend.Instance;
+            Label label = new Label();
+
+            label.Text = "test";
+
+            front.LoadMainLabel(label);
+
+            Assert.Equal(label, front.MainLabel);
+            Assert.Equal(label.Text, front.MainLabel.Text);
         }
     }
 }
