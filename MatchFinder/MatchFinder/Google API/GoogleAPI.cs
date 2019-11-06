aed1f4ebce4a9ec969a743c60c3ec3ce36b49a5c
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-11-06 20:43:45 +0100

    GoogleAPI class

---
+++ MatchFinder/MatchFinder/Google API/GoogleAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace MatchFinder
+{
+    public class GoogleAPI
+    {
+        private string APIkeyString;
+
+        public GoogleAPI()
+        {
+            // SaveAPIkeyAsync(); // use it to save new API key
+            LoadAPIkeyAsync();
+        }
+
+        public async Task SaveAPIkeyAsync()
+        {
+            try
+            {
+                await SecureStorage.SetAsync("APIkey", "put API key here");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task LoadAPIkeyAsync()
+        {
+            try
+            {
+                var oauthToken = await SecureStorage.GetAsync("APIkey");
+                this.APIkeyString = oauthToken.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
