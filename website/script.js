1da798c66afa9a71c456343b8a786c5d8c7d793a
Pawel <pawel.tomaszewski@student.um.si>
2019-11-11 22:50:28 +0100

    Google Maps geolocation handling errors

--- website/script.js
+++ website/script.js
@@ -8,4 +8,30 @@ function createMap () {
   };
 
   map = new google.maps.Map(document.getElementById('map'), options);
+  infoWindow = new google.maps.InfoWindow;
+
+  // Google Maps handling errors
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (p) {
+      var position = {
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
+      };
+
+      infoWindow.setPosition(position);
+      infoWindow.setContent('Your location!');
+      infoWindow.open(map);
+      map.setCenter(position);
+    }, function () {
+      handleLocationError('Geolocation service failed', map.getCenter());
+    });
+  } else {
+    handleLocationError('No geolocation available.', map.getCenter());
+  }
+}
+
+function handleLocationError (content, position) {
+  infoWindow.setPosition(position);
+  infoWindow.setContent(content);
+  infoWindow.open(map);
 }
