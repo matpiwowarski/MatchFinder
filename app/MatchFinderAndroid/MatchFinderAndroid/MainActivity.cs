2799c329fa73ea41ceeac2b28e2b6cd421ddcf7b
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-11-15 16:35:01 +0100

    android project

---
+++ app/MatchFinderAndroid/MatchFinderAndroid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace MatchFinderAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button displayButton;
+        TextView addressText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            // code
+            displayButton = (Button)FindViewById(Resource.Id.displayButton);
+            addressText = (TextView)FindViewById(Resource.Id.addressText);
+
+            displayButton.Click += DisplayButton_Click;
+        }
+
+        private void DisplayButton_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
