7ab3e5b3bf7901c13f6a77d2d9b0ecbd1768c35a
viksaxd <viktorangelovski97@gmail.com>
2019-11-25 22:30:25 +0100

    asdfads

---
+++ app/MatchFinder/MatchFinder/RegisterLogin/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MatchFinder.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+           
+        }
+
+        private void InitialazeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
