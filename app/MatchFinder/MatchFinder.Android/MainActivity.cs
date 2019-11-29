2c898f7c0afc30cf0a428d38c60b8ed84eefbac7
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-11-29 20:29:13 +0100

    setting up map

--- app/MatchFinder/MatchFinder.Android/MainActivity.cs
+++ app/MatchFinder/MatchFinder.Android/MainActivity.cs
@@ -7,14 +7,63 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using MatchFinder.GoogleAPI;
+using Android;
 
 namespace MatchFinder.Droid
 {
     [Activity(Label = "MatchFinder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+        Manifest.Permission.AccessCoarseLocation,
+        Manifest.Permission.AccessFineLocation
+        };
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                }
+                else
+                {
+                    // Permissions already granted - display a message.
+                }
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            if (requestCode == RequestLocationId)
+            {
+                if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted))
+                {
+                    // Permissions granted - display a message.
+                }
+                else
+                {
+                    // Permissions denied - display a message.
+                }
+
+            }
+            else
+            {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -24,12 +73,6 @@ namespace MatchFinder.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- app/MatchFinder/MatchFinder/MainMap.cs
+++ app/MatchFinder/MatchFinder/MainMap.cs
@@ -8,8 +8,19 @@ namespace MatchFinder
     {
         public MainMap()
         {
-            //Map map = new Map();
-            //Content = map;
+            Map map = new Map();
+            
+            Content = map;
+
+            Pin pin = new Pin
+            {
+                Label = "Santa Cruz",
+                Address = "The city with a boardwalk",
+                Type = PinType.Place,
+                Position = new Position(36.9628066, -122.0194722)
+            };
+
+            map.Pins.Add(pin);
         }
     }
 }
