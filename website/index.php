4615061b7caa71c763c56b9e82449de116ba05ec
Pawel <pawel.tomaszewski@student.um.si>
2019-12-09 14:06:05 +0100

    SearchBar for google pleaces

--- website/index.php
+++ website/index.php
@@ -1,60 +1,46 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>MatchFinder</title>
-  <meta name="viewport" content="initial-scale=1.0">
-  <meta charset="utf-8">
-
-  <!-- Stylesheets. -->
-  <link rel="stylesheet" href="style.css">
-
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <h1 class="display-4">Match Finder!</h1>
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
       </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <div id="map"></div>
-      </div>
-    </div>
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button> 
-  </div>
- 
+  	<?php endif ?>
 
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dolor justo, mattis non dui quis, hendrerit varius diam. Aliquam interdum porttitor enim vitae tempus. Praesent libero risus, semper sed sagittis pellentesque, ornare vel dui. Proin ut volutpat turpis. Nulla scelerisque cursus finibus. Nullam ultrices erat vitae tellus rhoncus venenatis. Nulla ut massa vitae libero volutpat pulvinar in ut justo. Aenean suscipit imperdiet blandit. Curabitur sodales dictum nibh, vel hendrerit lacus pulvinar non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce fringilla sodales mi eget pretium. Sed eu erat et magna rhoncus maximus a eget arcu. Vivamus eu lorem erat. In id molestie dolor. Integer sed viverra quam, at suscipit augue. Pellentesque quis nisl non ante efficitur pretium.
-
-Morbi porta dolor nec ultrices efficitur. Praesent ullamcorper sem a purus sollicitudin, eget sodales metus euismod. Donec blandit ipsum a dolor ultricies, id placerat libero aliquam. Morbi ac nisi vehicula, lacinia ligula vitae, posuere nibh. In dapibus, ex sed condimentum tempor, ipsum justo cursus nibh, nec egestas risus lorem eu dolor. Praesent vitae eleifend nisl. Maecenas porta laoreet elementum. Suspendisse urna nibh, ultrices sed tempor sed, iaculis luctus lacus. Curabitur ultricies, mi vel gravida elementum, tortor quam porta enim, a suscipit orci lacus eu urna. Nam ac molestie nisi. Integer eget ligula ac erat vulputate faucibus. Maecenas efficitur libero et euismod aliquet. Mauris id ante quis arcu suscipit efficitur eu nec justo. Mauris in ante nibh. Mauris id lacus ultricies, porta elit eu, ultricies justo.
-
-Quisque dignissim, tortor at tincidunt congue, neque dui ornare nisi, a placerat sapien magna eget nibh. Duis vitae purus elit. Aliquam augue eros, dignissim ut sapien in, bibendum accumsan erat. Sed a tempor eros. Cras tincidunt venenatis magna eu vestibulum. Maecenas bibendum et orci eu mattis. Donec ut erat imperdiet, consequat risus non, sollicitudin neque. Nunc feugiat erat non risus interdum feugiat. Duis sem libero, consequat at nisl vitae, faucibus pellentesque velit. Suspendisse cursus diam a sodales finibus. Etiam quis arcu fringilla, pharetra odio vel, viverra metus. Ut sit amet faucibus nisl, ut ultricies velit.
-
-Aenean ornare ante ac maximus finibus. Mauris blandit, nulla ut pharetra dictum, neque elit blandit turpis, eget iaculis ex quam vitae lorem. In faucibus ante mollis, porta leo eu, dignissim justo. Ut cursus dapibus turpis, non sollicitudin leo luctus in. Nunc ornare risus a tristique maximus. Vivamus tristique cursus odio, eget pellentesque massa venenatis vel. Vestibulum bibendum tincidunt consequat. In tortor nisl, ultricies ut sollicitudin quis, rutrum sit amet mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer mauris dolor, maximus sit amet faucibus vel, posuere quis arcu. Aenean nec tincidunt sapien. Curabitur consequat sagittis ligula, eu lacinia massa feugiat in.
-
-Aliquam pretium efficitur eros, sed consectetur urna ultrices sit amet. Integer sit amet urna quis nunc laoreet interdum id sed sapien. Curabitur efficitur a ligula vitae fringilla. In fermentum nisl libero. Ut consequat accumsan dolor vitae vehicula. Sed sit amet vehicula quam. Duis faucibus leo tellus, quis pellentesque ipsum sagittis vitae. Nunc enim nunc, laoreet eget ornare quis, malesuada in odio. Proin congue nec quam sit amet tincidunt.
-
-Aenean vestibulum rhoncus massa, quis consectetur nibh rutrum vel. Quisque ipsum diam, hendrerit in risus eget, mattis hendrerit mi. Curabitur posuere a dolor eget ultrices. Nam id tortor justo. Phasellus gravida tempus quam, vestibulum elementum magna pretium nec. Duis in posuere turpis. Quisque sed purus quis ante elementum mattis sit amet id est. Mauris at euismod nisl. Fusce eleifend, dolor a congue condimentum, nunc diam mollis velit, quis imperdiet purus urna ac nisi. Mauris at commodo neque, eget efficitur lectus. Aenean vel pharetra ipsum. Vivamus commodo, diam eu eleifend facilisis, arcu nibh molestie libero, vel ultricies justo sem at augue. Nullam bibendum gravida urna sed finibus.
-
-Fusce volutpat enim vitae tempus facilisis. Mauris ut nibh placerat, sagittis tellus egestas, gravida erat. Phasellus sit amet felis mi. Morbi quis semper mi, at pharetra leo. Vestibulum rhoncus augue non faucibus fermentum. Nullam vehicula ultricies nisl, vel luctus eros sodales in. In efficitur dolor sit amet ante dictum, tempor facilisis odio pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-Aliquam efficitur euismod eros ac pretium. Praesent ligula odio, auctor et dolor feugiat, laoreet congue justo. Maecenas neque nisl, sagittis at cursus id, dapibus et libero. Nulla fermentum neque arcu. In hac habitasse platea dictumst. Nam vitae justo at quam gravida pulvinar. Maecenas tincidunt nibh nec lectus efficitur, et lacinia urna pulvinar. Nulla facilisi. Morbi non purus ultrices, pretium tellus vel, interdum dui. Etiam luctus porta auctor.
-
-Aliquam non faucibus leo, interdum pharetra nulla. Integer est lacus, convallis vitae tortor nec, laoreet lacinia ipsum. Sed ultrices, quam vitae varius mattis, odio erat aliquet diam, sit amet auctor ipsum eros a risus. Nullam dignissim lectus nulla. Maecenas sit amet sem tortor. Mauris aliquet, arcu ut viverra sollicitudin, lorem lacus tristique mauris, quis dictum tellus quam non dui. Proin mattis consectetur augue, non efficitur nibh vulputate ut. Integer elementum dictum orci, sed gravida erat scelerisque tristique. Integer dignissim enim ac nulla suscipit, sit amet luctus tortor ornare.
-
-Maecenas faucibus lacus id enim pretium, in vestibulum velit ultrices. Fusce lacinia porta mauris, in consectetur nunc. Nulla commodo, velit vel porttitor tristique, lacus quam auctor diam, quis efficitur diam felis sed dui. Nam tortor neque, suscipit quis mollis et, accumsan id libero. Etiam nec sapien eget enim volutpat congue at vel felis. Suspendisse potenti. Sed blandit euismod erat sed malesuada. Integer feugiat est vitae sapien iaculis commodo. Nulla quis dignissim elit, sit amet rhoncus arcu. Vestibulum bibendum convallis interdum. Suspendisse et dolor sem. Suspendisse at elit ac orci tempus mollis eu quis dui. Aenean tristique sagittis condimentum.
-
-Fusce fringilla imperdiet ullamcorper. Praesent leo sapien, imperdiet vitae scelerisque in, luctus eu felis. Aliquam convallis, sem a commodo eleifend, mauris nulla facilisis nulla, quis venenatis odio nibh ut purus. Aliquam nec augue eu nunc maximus gravida. Aliquam tincidunt velit interdum massa eleifend, sit amet laoreet nisi dictum. Fusce sit amet massa dui. Maecenas ut dictum leo. Nullam molestie nibh vel orci posuere, et auctor eros scelerisque. Ut lacinia enim ac facilisis malesuada. Vestibulum quis erat facilisis, molestie odio a, rutrum lectus. Quisque vel sodales diam.
-
-Nam bibendum placerat cursus. Pellentesque placerat orci vitae erat tristique, non convallis arcu euismod. Donec elementum tellus sem, nec varius erat egestas a. Mauris a nisi magna. Donec lacus odio, vestibulum a eros non, imperdiet imperdiet neque. Maecenas a erat elit. Duis ut turpis velit. Vivamus lobortis, metus id rhoncus volutpat, lacus sem mollis enim, id efficitur eros justo vitae erat. Fusce ultrices massa massa, ac imperdiet purus pharetra sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur eget eros et ex iaculis porttitor nec pharetra dui. 
-  <!-- Google Maps scripts. -->
-  <script src="script.js"></script>
-  <!-- ScrollButton scripts. -->
-  <script src="scrollButton.js"></script>
-  <!-- Google API KEY-->
-  <script src="https://maps.googleapis.com/maps/api/js?callback=createMap&key=" async defer></script>
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+		
 </body>
 </html>
---
+++ website/webpage/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Match Finder</title>
+  <meta name="viewport" content="initial-scale=1.0">
+  <meta charset="utf-8">
+
+  <!-- Stylesheets. -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="container-fluid">
+  <div class="row">
+      <div class="col">
+        <h1 class="display-4">Match Finder</h1>
+      </div>
+    </div>
+    
+    <div class="row mb-2">
+      <div class="col">
+        <input class="form-control" id="search" type+"text" placeholder="Search place..."/>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col">
+        <div id="map"></div>
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button> 
+      </div>
+    </div>
+  </div>
+  <p id="demo"></p>
+  <div id="test"> Pawel </div>
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssss
+ ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+  <!-- Bootstrap scripts. -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <!-- Google Maps scripts. -->
+  <script src="script.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=APIKEYHERE&callback=createMap&libraries=places" async defer></script>
+  <!-- scroll button script -->
+  <script src="scrollButton.js"></script>
+
+</body>
+</html>
