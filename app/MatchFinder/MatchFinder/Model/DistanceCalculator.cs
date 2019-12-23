e02fd3ce9738880bb9610fdb68c75af8d46afe55
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-12-23 15:11:08 +0100

    Distance calculator class

---
+++ app/MatchFinder/MatchFinder/Model/DistanceCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace MatchFinder.Model
+{
+    public class DistanceCalculator
+    {
+        public DistanceCalculator()
+        {
+
+        }
+
+        // distance 
+
+        public double getDistance(double latitudeStart, double longitudeStart,
+            double latitudeEnd, double longitudeEnd)
+        {
+            return Location.CalculateDistance(latitudeStart, longitudeStart, latitudeEnd, longitudeEnd, DistanceUnits.Kilometers);
+        }
+
+        public double getDistance(Location locationStart, Location locationEnd)
+        {
+            return Location.CalculateDistance(locationStart, locationEnd, DistanceUnits.Kilometers);
+        }
+
+        public double getDistance(Location locationStart, double latitudeEnd, double longitudeEnd)
+        {
+            return Location.CalculateDistance(locationStart, latitudeEnd, longitudeEnd, DistanceUnits.Kilometers);
+        }
+
+        public double getDistance(double latitudeEnd, double longitudeEnd, Location locationStart)
+        {
+            return Location.CalculateDistance(locationStart, latitudeEnd, longitudeEnd, DistanceUnits.Kilometers);
+        }
+    }
+}
