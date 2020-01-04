31b038fa1e68e2466e1782eda66fc30799c28edf
Pawel <pawel.tomaszewski@student.um.si>
2020-01-04 14:15:04 +0100

    Display stadium location on map in Austria [website]

--- yii framework/frontend/web/js/script.js
+++ yii framework/frontend/web/js/script.js
@@ -20,6 +20,7 @@ function createMap () {
 
   var markers = [];
   
+
   // For search suggestions
   searchBox.addListener('places_changed', function () {
     // take searched place
@@ -36,7 +37,7 @@ function createMap () {
 
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(p) {
-      // if we don not have data for place(lan,lon)
+      // if we do not have data for place(lan,lon)
       if (!p.geometry)
         return;
       
@@ -69,4 +70,57 @@ function createMap () {
     map.fitBounds(bounds);
     
   });
-}  
+  
+
+document.getElementById("showAllStadiums").addEventListener("click", function(){
+
+  $.get('index.php?r=stadium/stadium-array-json&countryName=Austria&json=true)', function(data)
+    {
+        stadiumArray = JSON.parse(data);
+
+        var i;
+        for (i = 0; i < stadiumArray.length; i++) {
+      markers.push(new google.maps.Marker({
+      map: map,
+      title: stadiumArray[i].street,
+      position:{lat: parseFloat(stadiumArray[i].latitude), lng: parseFloat(stadiumArray[i].longitude)},
+    }));;
+    }
+    var bounds = new google.maps.LatLngBounds();
+
+   bounds.extend({lat: parseFloat(stadiumArray[1].latitude), lng: parseFloat(stadiumArray[1].longitude)});
+   
+   map.setCenter(bounds.getCenter());
+  //  map.fitBounds(bounds);
+  zoomChangeBoundsListener = 
+    google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) {
+        if ( this.getZoom() ){   // or set a minimum
+            this.setZoom(6);  // set zoom here
+        }
+});
+
+setTimeout(function(){google.maps.event.removeListener(zoomChangeBoundsListener)}, 2000);
+  }
+  )
+});
+
+}
+ 
+
+// function getStadiumJson(countryName)
+// {
+//     $.get('index.php?r=stadium/stadium-array-json&countryName=' + countryName + '&json=true)', function(data)
+//     {
+//         stadiumArray = JSON.parse(data);
+//         window.alert(stadiumArray[0].latitude);
+//     }
+//     )
+// }
+
+// var marker = new google.maps.Marker({
+//   map: map,
+//   title: "salzburg",
+//   position: {lat: parseFloat(stadiumArray[0].latitude), lng: parseFloat(stadiumArray[0].longitude)}
+// })
+// }
+// )
