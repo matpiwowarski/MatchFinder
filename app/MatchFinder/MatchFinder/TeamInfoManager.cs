e1523d0aa535acd57290358cec24a922f8fd8b0f
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-01-10 00:33:57 +0100

    switch with team info works

--- app/MatchFinder/MatchFinder/TeamInfoManager.cs
+++ app/MatchFinder/MatchFinder/TeamInfoManager.cs
@@ -27,9 +27,45 @@ namespace MatchFinder
             {
                 this.teamName.Text = "NK Olimpija Ljubljana";
             }
+            else if (teamName == "Olimpija NK")
+            {
+                this.teamName.Text = "NK Olimpija Ljubljana";
+            }
             else if (teamName == "NK Maribor")
             {
                 this.teamName.Text = "NK Maribor";
+            }
+            else if (teamName == "Bravo")
+            {
+                this.teamName.Text = "NK Bravo";
+            }
+            else if (teamName == "Tabor Sežana")
+            {
+                this.teamName.Text = "NK Tabor Sežana";
+            }
+            else if (teamName == "Celje")
+            {
+                this.teamName.Text = "NK Celje";
+            }
+            else if (teamName == "Aluminij")
+            {
+                this.teamName.Text = "NK Aluminij";
+            }
+            else if (teamName == "Domzale")
+            {
+                this.teamName.Text = "NK Domžale";
+            }
+            else if (teamName == "Mura")
+            {
+                this.teamName.Text = "NŠ Mura";
+            }
+            else if (teamName == "Triglav")
+            {
+                this.teamName.Text = "NK Triglav Kranj";
+            }
+            else if (teamName == "Rudar Velenje")
+            {
+                this.teamName.Text = "NK Rudar Velenje";
             }
         }
     }
