b81649bab9f90dcd0694739e087de732da61d77d
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-01-09 21:12:30 +0100

    deleted NavigationPage to delete blue rectangle

--- app/MatchFinder/MatchFinder/App.xaml.cs
+++ app/MatchFinder/MatchFinder/App.xaml.cs
@@ -11,8 +11,8 @@ namespace MatchFinder
         {
             InitializeComponent();
 
-            //MainPage = new MainPage();
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new MainPage();
+            //MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
